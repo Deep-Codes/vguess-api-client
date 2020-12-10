f3afad897e6e971821458cc4bdb898e1a7508c39
Deepankar <61158210+Deep-Codes@users.noreply.github.com>
2020-12-11 02:11:30 +0530

    🚀 Fields Added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,16 @@ import React, { useState } from 'react';
 import './App.css';
 import logo from './assets/logo.png';
 
+interface resQuestion {
+  word: string;
+  hint: string;
+}
+
 function App() {
   const [select, setSelect] = useState<String>('java');
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<null | Array<resQuestion>>(null);
+  const [word, setWord] = useState<string>('');
+  const [hint, setHint] = useState<string>('');
 
   const showData = () => {
     console.log('req send');
@@ -15,31 +22,60 @@ function App() {
     });
     console.log(data);
   };
+
+  const addQuestion = () => {
+    if (hint && word !== '') {
+      console.log(hint, word);
+    }
+  };
   return (
     <div className='App'>
       <img src={logo} alt='Logo Vguess' />
 
-      <select name='languages' onChange={(e) => setSelect(e.target.value)}>
+      <select
+        name='languages'
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+          setSelect(e.target.value)
+        }
+      >
         <option value='java'>Java</option>
         <option value='javascript'>javascript</option>
         <option value='python'>Python</option>
       </select>
-
-      {data === [] ? (
-        <div>Loading Data ... </div>
-      ) : (
-        <div>
-          <h1>{select.toUpperCase()} Questions</h1>
-          {data.map((dt: any) => (
-            <div className='cell'>
-              {dt.word}
-              {dt.hint}
-            </div>
-          ))}
-        </div>
-      )}
-
       <button onClick={showData}>Show</button>
+
+      <input
+        type='text'
+        value={word}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setWord(e.target.value)
+        }
+        placeholder='Enter Word'
+      />
+      <input
+        type='text'
+        value={hint}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setHint(e.target.value)
+        }
+        placeholder='Enter Hint'
+      />
+
+      <button onClick={addQuestion}>Add Question</button>
+
+      {data && (
+        <>
+          <div>
+            <h1>{select.toUpperCase()} Questions</h1>
+            {data.map((dt: resQuestion) => (
+              <div className='cell'>
+                {dt.word}
+                {dt.hint}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
